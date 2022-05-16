3173b32a4ea062a2196767acff58c54aaa2b8663
gearnode <bryan.frimin@exograd.com>
2022-05-16 14:10:11 +0200

    Add OAuth2 error struct

---
+++ error.go
@@ -0,0 +1,32 @@
+// Copyright (c) 2022 Bryan Frimin <bryan@frimin.fr>.
+//
+// Permission to use, copy, modify, and/or distribute this software for
+// any purpose with or without fee is hereby granted, provided that the
+// above copyright notice and this permission notice appear in all
+// copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+// WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
+// AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
+// DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR
+// PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+// TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+// PERFORMANCE OF THIS SOFTWARE.
+
+package oauth2c
+
+type Error struct {
+	// A single ASCII error code.
+	Code string `json:"error"`
+
+	// Human-readable ASCII text providing additional information,
+	// used to assist the client developer in understanding the
+	// error that occurred.
+	Description string `json:"error_description"`
+
+	// A URI identifying a human-readable web page with information
+	// about the error, used to provide the client developer with
+	// additional information about the error
+	URI string `json:"error_uri"`
+}
