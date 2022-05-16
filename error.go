ec7d6c4c78968eff67e598784835f6a416f1d336
gearnode <bryan.frimin@exograd.com>
2022-05-16 14:39:19 +0200

    Use real name of error field

--- error.go
+++ error.go
@@ -18,15 +18,15 @@ package oauth2c
 
 type Error struct {
 	// A single ASCII error code.
-	Code string `json:"error"`
+	Error string `json:"error"`
 
 	// Human-readable ASCII text providing additional information,
 	// used to assist the client developer in understanding the
 	// error that occurred.
-	Description string `json:"error_description"`
+	ErrorDescription string `json:"error_description"`
 
 	// A URI identifying a human-readable web page with information
 	// about the error, used to provide the client developer with
 	// additional information about the error
-	URI string `json:"error_uri"`
+	ErrorURI string `json:"error_uri"`
 }
